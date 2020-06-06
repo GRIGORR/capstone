7e15dda34f04b365ee112caf6bdb57ed6af700e4
GRIGORR <grigor_nalbandyan@edu.aua.am>
2020-06-06 17:44:27 +0400

    Minor fix

--- get_dataset_with_transform.py
+++ get_dataset_with_transform.py
@@ -1,9 +1,9 @@
-import torch
 import numpy as np
+import json
+import torch
 import torchvision.datasets as dset
 import torchvision.transforms as transforms
-import json
-from SearchDatasetWrap import SearchDataset
+import torch.utils.data as data
 
 
 class CUTOUT(object):
@@ -58,3 +58,32 @@ def get_nas_search_loaders(train_data, config_root, batch_size, workers):
     search_loader = torch.utils.data.DataLoader(search_data, batch_size=batch_size, shuffle=True, num_workers=workers,
                                                 pin_memory=True)
     return search_loader
+
+
+class SearchDataset(data.Dataset):
+
+    def __init__(self, data, train_split, valid_split):
+        self.datasetname = 'cifar10'
+        self.data = data
+        self.train_split = train_split
+        self.valid_split = valid_split
+        intersection = set(train_split).intersection(set(valid_split))
+        assert len(intersection) == 0, 'the splitted train and validation sets should have no intersection'
+        self.length = len(self.train_split)
+
+    def __repr__(self):
+        return ('{name}(name={datasetname}, train={tr_L}, valid={val_L}, version={ver})'.format(
+            name=self.__class__.__name__, datasetname=self.datasetname, tr_L=len(self.train_split),
+            val_L=len(self.valid_split), ver=self.mode_str))
+
+    def __len__(self):
+        return self.length
+
+    def __getitem__(self, index):
+        assert index >= 0 and index < self.length, 'invalid index = {:}'.format(index)
+        train_index = self.train_split[index]
+        valid_index = np.random.choice(self.valid_split)
+        train_image, train_label = self.data[train_index]
+        valid_image, valid_label = self.data[valid_index]
+
+        return train_image, train_label, valid_image, valid_label
